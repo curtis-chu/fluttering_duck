5870691fcb1abdffc90c2a554624505d77778961
Curtis Chu <curtis.chu@wonderpet.asia>
2022-08-29 22:07:51 +0800

    add duck

--- lib/main.dart
+++ lib/main.dart
@@ -69,6 +69,11 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
+    double appWidth = MediaQuery.of(context).size.width;
+    double appHeight = MediaQuery.of(context).size.height;
+    double duckWidth = 120.0;
+    double duckHeight = 120.0;
+
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
@@ -84,31 +89,22 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
         width: double.infinity,
         height: double.infinity,
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+        child: Stack(
+          alignment: Alignment.topLeft,
+          children: [
+            Positioned(
+              left: (appWidth * 0.3) - (duckWidth / 2),
+              top: (appHeight * 0.5) - (duckHeight / 2),
+              child: Container(
+                width: duckWidth,
+                height: duckHeight,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("images/live_duck.png"),
+                    fit: BoxFit.scaleDown,
+                  ),
+                ),
+              ),
             ),
           ],
         ),
